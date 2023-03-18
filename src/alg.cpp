0ff1212def023cd81d0baa977cdaa240dfe96419
DmitriiObushko <105298559+DmitriiObushko@users.noreply.github.com>
2023-03-18 21:49:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int cbinsearch(int *arr, int size, int value) {
   int count = 0;
     for (int i = 0; i < size; ++i) {
         if (arr[i] == value) {
-            count = count + 1;
+            count += 1;
         }
     }
  return count;
